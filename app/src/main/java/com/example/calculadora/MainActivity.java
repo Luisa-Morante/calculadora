38c54d2a8c1d777ee0a672d37f46725fda56b6c7
luisa-morante <luisa.morante@mail.udp.cl>
2020-06-18 13:59:44 -0400

    cambios

---
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+         android:color="#0074BD"
